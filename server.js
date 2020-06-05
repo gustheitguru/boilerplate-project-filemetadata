2c842909799a3ed8a2b9f85585add83776fcd9d6
Gus <gusr@logicliving.com>
2020-06-04 20:28:28 -0500

    finished project
    finished project

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //new project
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
-
+let express = require('express');
+let cors = require('cors');
+let prettyBytes = require('pretty-bytes'); //to convert file size
+let multer = require('multer');
+let upload = multer({ dest: 'assets/' });
 // require and use "multer"...
 
 var app = express();
@@ -18,6 +20,17 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  // req.file is the `upfile` file
+  // req.body will hold the text fields, if there were any
+  let fileName = req.file.originalname;
+  let fileSize = req.file.size;
+  let readSize = prettyBytes(fileSize, {bits: true});
+  
+  res.json({"fileName": fileName, "size":readSize, "sizeInBytes": fileSize});
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
